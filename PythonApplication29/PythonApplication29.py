80ee747cf166de0f84c6374f484f8c467162f05e
Jacob <jacob@stanislaus.co.uk>
2021-09-30 13:09:18 +0100

    Error Validation implemented

--- PythonApplication29/PythonApplication29.py
+++ PythonApplication29/PythonApplication29.py
@@ -3,7 +3,7 @@ from debugprint import Debug
 import time
 loop = True
 global dim3
-while loop == True:
+def func1():
     i = 0
     array = []
     attempt2 = 0
@@ -33,7 +33,7 @@ while loop == True:
     store7 = []
     store8 = []
     holder = 0
-  
+    dim = []
     code1= True
     n =0
     idk = 0
@@ -49,12 +49,21 @@ while loop == True:
     x = str("arr")
     z = str("arr")
     print("\nThis is a matrix generator")
-    menu = int(input("1. Choose matrices' dimensions \n2. Random dimensions\n3. Can you multiply these matrices?\n4. Multiplication with matrices practice\n5. Find the determinant of this random matrix\n6.Find the Inverse of a matrix\n\n"))
+    menu = input("1. Choose matrices' dimensions \n2. Random dimensions\n3. Can you multiply these matrices?\n4. Multiplication with matrices practice\n5. Find the determinant of this random matrix\n6.Find the Inverse of a matrix\n\n")
+    if menu.isdigit() and 1<= int(menu) <=7:
+        menu = int(menu)
+    else:
+        print("\nError, Please enter a number from 1 to 6")
+        time.sleep(1)
+        func1()
     if menu == 1:
         dim = input("Please enter the dimensions you want for the matrix with a space between each character\n")
         dim = dim.split(" ")
-        if isinstance(dim[0], int):
-            print("Please enter in the form such as 3 x 4 ")
+        while True:
+            if dim[0].isdigit() and dim[2].isdigit and dim[1]=="x":
+                break
+            else:
+                print("Please enter a valid input such as 3 x 2 with a space between each character")
         for i in range (0,int(dim[0])):
             y = str(i)
             x += y
@@ -64,45 +73,85 @@ while loop == True:
                 if len(x) == int(dim[2]):
                     print(x)
     elif menu == 2:
-        l = int(input("What do you want the max number of rows or columns to be? "))
+        while True:
+            l = input("What do you want the max number of rows or columns to be? ")
+            if l.isdigit():
+                l = int(l)
+                break
+            else:
+                print("\nPlease enter a valid number")
+                func1()
+
+
         dim1 = random.randint(2,l)
-        dim2 = random.randint(2,l)
+        dim2 = random.randint(2,l)#
+        while True:
+            maxsize = input("What do you want the maximum number of digits per number to be?(2 or 1)")
+            if maxsize.isdigit() and 1 <= int(maxsize) <=2:
+                maxsize = int(maxsize)
+                break
+            else:
+                print("Please enter a valid number")
+        if maxsize == 1:
+            digit = 10
+        elif maxsize == 2:
+            digit = 100
         for i in range (0,int(dim1)):
             y = str(i)
             x += y
             x = []
             for j in range (0,int(dim2)):
-                (x).append(random.randint(0,9))
+                (x).append(random.randint(0,digit))
                 if len(x) == int(dim2):
                     print(x)        
 
     elif menu == 3:
         que = input("Would you like to choose the dimensions?\n\n")
-        if que == "Yes":    
-            dim = input("Please enter the dimensions you want for the matrix with a space between each character\n")
-            dim = dim.split(" ")
+        if que == "Yes":
+            while True:
+                dim = input("Please enter the dimensions you want for the matrix with a space between each character\n")
+                dim = dim.split(" ")
+                if dim[0].isdigit() and dim[1]=="x"and dim[2].isdigit():
+                    l = int(l)
+                    break
+                else:
+                    print("\nPlease enter in the correct form such as 3 x 4 ")
+            while True:
+                maxsize = input("What do you want the maximum number of digits per number to be?(2 or 1)")
+                if maxsize.isdigit() and 1 <= int(maxsize) <=2:
+                    maxsize = int(maxsize)
+                    break
+                else:
+                    print("Please enter a valid number")
+            if maxsize == 1:
+                digit = 10
+            elif maxsize == 2:
+                digit = 100
 
-            if isinstance(dim[0], int):
-                print("Please enter in the form such as 3 x 4 ")
             for i in range (0,int(dim[0])):
                 y = str(i)
                 x += y
                 x = []
                 for j in range (0,int(dim[2])):
-                    (x).append(random.randint(0,9))
+                    (x).append(random.randint(0,digit))
                     if len(x) == int(dim[2]):
                         array.append(x)
-            dim1 = input("Please enter the dimensions you want for the matrix with a space between each character\n")
-            dim1 = dim1.split(" ")
-            if isinstance(dim1[0], int):
-                print("Please enter in the form such as 3 x 4 ")
+            while True:
+                dim1 = input("Please enter the dimensions you want for the matrix with a space between each character\n")
+                dim1 = dim1.split(" ")
+                if dim[0].isdigit() and dim[1]=="x"and dim[2].isdigit():
+                    l = int(l)
+                    break
+                else:
+                    print("\nPlease enter in the correct form such as 3 x 4 ")
+
  
             for i in range (0,int(dim1[0])):
                 y = str(i)
                 x += y
                 x = []
                 for j in range (0,int(dim1[2])):
-                    (x).append(random.randint(0,9))
+                    (x).append(random.randint(0,digit))
                     if len(x) == int(dim1[2]):
                         store4.append(x)
             #dim[0] == dims
@@ -144,7 +193,24 @@ while loop == True:
         elif que == "No":
             w = 0
             while code == True:
-                l = int(input("What do you want the max number of rows or columns to be? "))
+                while True:
+                    l = input("What do you want the max number of rows or columns to be? ")
+                if l.isdigit():
+                    l = int(l)
+                    break
+                else:
+                    print("\nPlease enter a valid number")
+                while True:
+                    maxsize = input("What do you want the maximum number of digits per number to be?(2 or 1)")
+                    if maxsize.isdigit() and 1 <= int(maxsize) <=2:
+                        maxsize = int(maxsize)
+                        break
+                    else:
+                        print("Please enter a valid number")
+                if maxsize == 1:
+                    digit = 10
+                elif maxsize == 2:
+                    digit = 100
                 #makes num of rows
                 for brrr in range (0,2):
                     if brrr == 0:
@@ -159,7 +225,7 @@ while loop == True:
                             x = []
                             for j in range (0,int(dim3)):
                                 #adds nums to arrays/ rows
-                                (x).append(random.randint(0,9))
+                                (x).append(random.randint(0,digit))
                                 if len(x) == int(dim3):
                                     array.append(x) 
                     elif brrr == 1:
@@ -174,18 +240,17 @@ while loop == True:
                             x = []
                             for j in range (0,int(dim2)):
                                 #adds nums to arrays/ rows
-                                (x).append(random.randint(0,9))
+                                (x).append(random.randint(0,digit))
                                 if len(x) == int(dim2):
                                     store4.append(x) 
 
 
                 
-                i=0
                 for i in range (0,len(array)):
                     if len(array[i]) > dim3:
                         array[i].pop()
                     print(array[i])
-                i=0
+
                 print("\n\n")
                 for i in range (0,len(store4)):
                     if len(store4[i]) > dim2:
@@ -216,9 +281,12 @@ while loop == True:
         if que == "Yes":
             dim = input("Please enter the dimensions you want for the matrix with a space between each character\n")
             dim = dim.split(" ")
+            while True:
+                if dim[0].isdigit() and dim[2].isdigit and dim[1]=="x":
+                    break
+                else:
+                    print("Please enter a valid input such as 3 x 2 with a space between each character")
 
-            if isinstance(dim[0], int):
-                print("Please enter in the form such as 3 x 4 ")
             for i in range (0,int(dim[0])):
                 y = str(i)
                 x += y
@@ -229,15 +297,29 @@ while loop == True:
                         array.append(x)
             dim1 = input("Please enter the dimensions you want for the 2nd matrix with a space between each character\n")
             dim1 = dim1.split(" ")
-            if isinstance(dim1[0], int):
-                print("Please enter in the form such as 3 x 4 ")
+            while True:
+                if dim1[0].isdigit() and dim1[2].isdigit and dim1[1]=="x":
+                    break
+                else:
+                    print("Please enter a valid input such as 3 x 2 with a space between each character")
+            while True:
+                maxsize = input("What do you want the maximum number of digits per number to be?(2 or 1)")
+                if maxsize.isdigit() and 1 <= int(maxsize) <=2:
+                    maxsize = int(maxsize)
+                    break
+                else:
+                    print("Please enter a valid number")
+            if maxsize == 1:
+                digit = 10
+            elif maxsize == 2:
+                digit = 100
             if dim1[0] == dim[2] and dim[0] == dim1[2] :
                 for i in range (0,int(dim1[0])):
                     y = str(i)
                     x += y
                     x = []
                     for j in range (0,int(dim1[2])):
-                        (x).append(random.randint(0,9))
+                        (x).append(random.randint(0,digit))
                         if len(x) == int(dim1[2]):
                             store4.append(x)
             else:
@@ -341,7 +423,17 @@ while loop == True:
                             if dim2 == dims:
                                 code1 = False
 
-
+                while True:
+                    maxsize = input("What do you want the maximum number of digits per number to be?(2 or 1)")
+                    if maxsize.isdigit() and 1 <= int(maxsize) <=2:
+                        maxsize = int(maxsize)
+                        break
+                    else:
+                        print("Please enter a valid number")
+                if maxsize == 1:
+                    digit = 10
+                elif maxsize == 2:
+                    digit = 100
                 #makes num of rows
                 for brrr in range (0,2):
                     if brrr == 0:
@@ -353,7 +445,7 @@ while loop == True:
                             x = []
                             for j in range (0,int(dim3)):
                                 #adds nums to arrays/ rows
-                                (x).append(random.randint(0,9))
+                                (x).append(random.randint(0,digit))
                                 if len(x) == int(dim3):
                                     array.append(x)
                     elif brrr == 1:
@@ -366,7 +458,7 @@ while loop == True:
                             x = []
                             for j in range (0,int(dim2)):
                                 #adds nums to arrays/ rows
-                                (x).append(random.randint(0,9))
+                                (x).append(random.randint(0,digit))
                                 if len(x) == int(dim2):
                                     store4.append(x)
 
@@ -448,15 +540,29 @@ while loop == True:
     if menu == 5:
         dim = input("\nPlease enter the dimensions you want for the matrix with a space between each character (either 2 x 2 or 3 x 3)\n")
         dim = dim.split(" ")
-        if int(dim[0]) == int(dim[2]):
-            if isinstance(dim[0], int):
-                print("Please enter in the form such as 3 x 3 ")
+        while True:
+            if dim[0].isdigit() and dim[1]=="x"and dim[2].isdigit():
+                l = int(l)
+                break
+            else:
+                print("\nPlease enter in the correct form such as 3 x 4 ")
+            while True:
+                maxsize = input("What do you want the maximum number of digits per number to be?(2 or 1)")
+                if maxsize.isdigit() and 1 <= int(maxsize) <=2:
+                    maxsize = int(maxsize)
+                    break
+                else:
+                    print("Please enter a valid number")
+            if maxsize == 1:
+                digit = 10
+            elif maxsize == 2:
+                digit = 100
             for i in range (0,int(dim[0])):
                 y = str(i)
                 x += y
                 x = []
                 for j in range (0,int(dim[2])):
-                    (x).append(random.randint(0,9))
+                    (x).append(random.randint(0,digit))
                     if len(x) == int(dim[2]):
                         print(x)
                         store1.append(x)
@@ -492,8 +598,28 @@ while loop == True:
     if menu == 6:
         dim = input("\nPlease enter the dimensions you want for the matrix with a space between each character (either 2 x 2 or 3 x 3)\n")
         dim = dim.split(" ")
+        while True:
+            if dim[0].isdigit() and dim[1]=="x"and dim[2].isdigit():
+                l = int(l)
+                break
+            else:
+                print("\nPlease enter in the correct form such as 3 x 4 ")
+        while True:
+            maxsize = input("What do you want the maximum number of digits per number to be?(2 or 1)")
+            if maxsize.isdigit() and 1 <= int(maxsize) <=2:
+                maxsize = int(maxsize)
+                break
+            else:
+                print("Please enter a valid number")
+        if maxsize == 1:
+            digit = 10
+        elif maxsize == 2:
+            digit = 100
         print("\n")
         Debug("Hello")
+        if int(dim[0])>3 or int(dim[0])<2:
+            print("Please enter either 2 x 2 or 3 x 3 in that form")
+            func1()
         if int(dim[0]) == int(dim[2]):
             if isinstance(dim[0], int):
                 print("Please enter in the form such as 3 x 3 ")
@@ -503,7 +629,7 @@ while loop == True:
                 x += y
                 x = []
                 for j in range (0,int(dim[2])):
-                    (x).append(random.randint(0,9))
+                    (x).append(random.randint(0,digit))
                     if len(x) == int(dim[2]):
                         print(x)
                         store1.append(x)
@@ -513,7 +639,7 @@ while loop == True:
             print("You can only find the Inverse of a square matrix ")
         if int(dim[0]) ==2:
             determin = (store1[0][0]*store1[1][1])-(store1[0][1]*store1[1][0])
-        if int(dim[0]) == 3:
+        elif int(dim[0]) == 3:
             for i in range(0,len(x)):
                 #pack = [1,2]
                 #pack2 = [0,1,2]
@@ -580,6 +706,7 @@ while loop == True:
             y3.append(x2[2])
             y3.append(x3[2])
                # print("x3 is "+str(x3))
+
         for i in range(0,len(x)):
             pack = [1,2]
             pack2 = [0,1,2]
@@ -595,6 +722,30 @@ while loop == True:
             print(" = "+str(sumdet))
             x4.append(sumdet)
         determin = int(x4[0])-int(x4[1])+int(x4[2])
+        det = int(input("What is the determinant for this matrix? "))
+        if det == determin:
+            print("correct")
+        else:
+            print("incorrect, it is "+str(determin))
+        for i in range(0,len(y1)):
+            attempt = int(input("What is the num "+str(i+1)+" element of the matrix? "))
+            if attempt == y1[i]:
+                print("correct")
+            else:
+                print("Incorrect, it should be "+str(y2[i]))
+        for i in range(0,len(y2)):
+            attempt = int(input("What is the num "+str(i+1)+" element of the matrix? "))
+            if attempt == y2[i]:
+                print("correct")
+            else:
+                print("Incorrect, it should be "+str(y2[i]))
+        for i in range(0,len(y3)):
+            attempt = int(input("What is the num "+str(i+1)+" element of the matrix? "))
+            if attempt == y3[i]:
+                print("correct")
+            else:
+                print("Incorrect, it should be "+str(y3[i]))
+            
         print("\n\nThe inverse is ")
         if 100 >determin > 10:
             print("1      " + str(y1) + "\n---"+ " X  "+ str(y2)+ "\n"+str(determin)+"     "+str(y3))
@@ -665,5 +816,7 @@ while loop == True:
             elif determin < 10:
                 print("1      " + str(store1) + "\n---"+ " X  "+ str(store2)+ "\n"+str(determin))
 
-                 
+      
 
+while True:
+    func1()
